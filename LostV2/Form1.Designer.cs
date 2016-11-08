e87be4c71d57c0f188320f07ff69c97becc5384c
wahhe <yuanhe326@ed.amdsb.ca>
2016-11-08 16:20:10 -0500

    Add project files.

---
+++ LostV2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace LostV2
+{
+    partial class Donut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Donut));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.blueImage = new System.Windows.Forms.PictureBox();
+            this.redImage = new System.Windows.Forms.PictureBox();
+            this.yellowImage = new System.Windows.Forms.PictureBox();
+            this.yellowlabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(24, 28);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(339, 257);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(67, 313);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.TabIndex = 3;
+            this.redLabel.Text = "label1";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(67, 349);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(257, 23);
+            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Text = "label1";
+            // 
+            // blueImage
+            // 
+            this.blueImage.Image = global::LostV2.Properties.Resources.blue_50x50;
+            this.blueImage.Location = new System.Drawing.Point(24, 339);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(2);
+            this.blueImage.Name = "blueImage";
+            this.blueImage.Size = new System.Drawing.Size(39, 32);
+            this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.blueImage.TabIndex = 1;
+            this.blueImage.TabStop = false;
+            // 
+            // redImage
+            // 
+            this.redImage.Image = global::LostV2.Properties.Resources.red_50x50;
+            this.redImage.Location = new System.Drawing.Point(24, 303);
+            this.redImage.Margin = new System.Windows.Forms.Padding(2);
+            this.redImage.Name = "redImage";
+            this.redImage.Size = new System.Drawing.Size(39, 32);
+            this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.redImage.TabIndex = 0;
+            this.redImage.TabStop = false;
+            // 
+            // yellowImage
+            // 
+            this.yellowImage.Image = global::LostV2.Properties.Resources.yellow_50x50;
+            this.yellowImage.Location = new System.Drawing.Point(24, 375);
+            this.yellowImage.Margin = new System.Windows.Forms.Padding(2);
+            this.yellowImage.Name = "yellowImage";
+            this.yellowImage.Size = new System.Drawing.Size(39, 32);
+            this.yellowImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yellowImage.TabIndex = 5;
+            this.yellowImage.TabStop = false;
+            // 
+            // yellowlabel
+            // 
+            this.yellowlabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowlabel.ForeColor = System.Drawing.Color.White;
+            this.yellowlabel.Location = new System.Drawing.Point(67, 384);
+            this.yellowlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.yellowlabel.Name = "yellowlabel";
+            this.yellowlabel.Size = new System.Drawing.Size(257, 23);
+            this.yellowlabel.TabIndex = 6;
+            this.yellowlabel.Text = "label1";
+            // 
+            // Donut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.Controls.Add(this.yellowlabel);
+            this.Controls.Add(this.yellowImage);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.blueImage);
+            this.Controls.Add(this.redImage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Donut";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buy Donut For Mr.Steel";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox redImage;
+        private System.Windows.Forms.PictureBox blueImage;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.PictureBox yellowImage;
+        private System.Windows.Forms.Label yellowlabel;
+    }
+}
+
---
+++ LostV2/Form1.cs
@@ -0,0 +1,146 @@
+﻿/// created by : Wah He
+/// date       : Nov. 6
+/// description: A basic text adventure game engine
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LostV2
+{
+    public partial class Donut : Form
+    {
+        int scene = 0;  // tracks what part of the game the user is at
+        Random randGen = new Random();
+        public Donut()
+        {
+            InitializeComponent();
+
+            //display initial message and options
+            outputLabel.Text = "One day, Mr.Steel asked you to get him a donut. Will you go for him?";
+            redLabel.Text = "Yes";
+            blueLabel.Text = "No";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+                else if (scene == 4)
+                {
+                    scene = 5;
+                }
+                else if (scene == 6)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 6)
+                {
+                    int chance = randGen.Next(1, 101);
+                    if (chance < 80)
+                    {
+                        scene = 8;
+                    }
+                    else
+                    {
+                        scene = 12;
+                    }
+                }
+                else if (scene == 7)
+                {
+                    scene = 10;
+                }
+                else if (scene == 7)
+                {
+                    scene = 11;
+                }
+            }
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:  //start scene  
+                    outputLabel.Text = "One day, Mr.Steel asked you to get him a donut. \nWill you go for him?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 1:
+                    outputLabel.Text = "You went out from the classroom. \nWill you turn left or right? ";
+                    redLabel.Text = "Left";
+                    blueLabel.Text = "Right";
+                    break;
+                case 2:
+                    outputLabel.Text = "You got kicked out from his class. Play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 3:
+                    outputLabel.Text = "You got caught by Mr.Cassone. Play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 4:
+                    outputLabel.Text = "Mr. Armstrong is walking on the hallway. \nWill you go say hi to him or ignore and walk by? ";
+                    redLabel.Text = "Ignore and walk By";
+                    blueLabel.Text = "Go say hi to him";
+                    break;
+                case 5:
+                    outputLabel.Text = "Mr.Armstrong told Mr.Cassone that you are skipping classes. \nSo you got caught by Mr.Cassone. Play again? ";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 6:
+                    outputLabel.Text = "Mr. Armstrong likes to see people skipping Mr. Steel's classes.\nSo he wants to give u a $5 gift card for skipping Mr. Steel's math class.\nWill you accpect or refuse his kindness?";
+                    redLabel.Text = "Accept";
+                    blueLabel.Text = "Refuse";
+                    break;
+                case 7:
+                    outputLabel.Text = "You went to Timmy and got donut and coffee. \nWill you run back, walk back or just go home with the coffee and donut?";
+                    redLabel.Text = "Walk Back";
+                    blueLabel.Text = "Run Back";
+                    yellowlabel.Text = "Go Home eat the donut and drink the coffee";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+}
